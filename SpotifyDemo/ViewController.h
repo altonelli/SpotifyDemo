eb20f6e908c3432c9574ca0f874c3c845d69dba3
altonelli <altonelli14@gmail.com>
2016-07-21 11:18:38 -0700

    Initial Commit

---
+++ SpotifyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpotifyDemo
+//
+//  Created by Arthur Tonelli on 7/21/16.
+//  Copyright © 2016 Arthur Tonelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SpotifyDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SpotifyDemo
+//
+//  Created by Arthur Tonelli on 7/21/16.
+//  Copyright © 2016 Arthur Tonelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
